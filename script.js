fc7f02d891ad67aafe66c189ac5f3a759f9697df
shubham <shubhamagarwal0632@gmail.com>
2024-04-01 19:26:53 +0530

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let users = [
     {
         profilePic: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        displayPic: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1618085219724-c59ba48e08cd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         pendingMessage: "2",
         location: "pune,india",
         name: "laxita",
+        pendingMessagevalue: [
+
+            {
+                messageimg: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "hello1"
+            },
+            {
+                messageimg: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "namaste1"
+            },
+
+        ],
         age: 7,
         interests: [{
             icon: `<i class="ri-music-2-line"></i>`,
@@ -18,11 +30,23 @@ let users = [
         bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, unde!",
         isFriend: null
     }, {
-        profilePic: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        profilePic: "https://images.unsplash.com/photo-1618085219724-c59ba48e08cd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         displayPic: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         pendingMessage: "5",
         location: "delhi,india",
         name: "niharika",
+        pendingMessagevalue: [
+
+            {
+                messageimg: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "hello2"
+            },
+            {
+                messageimg: "https://images.unsplash.com/photo-1616065297556-f05bc00c9a3e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "namaste2"
+            },
+
+        ],
         age: 7,
         interests: [{
             icon: `<i class="ri-music-2-line"></i>`,
@@ -36,11 +60,22 @@ let users = [
         bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, unde!",
         isFriend: null
     }, {
-        profilePic: "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        profilePic: "https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         displayPic: "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         pendingMessage: "7",
         location: "goa",
-        name: "seenika",
+        name: "seenika", pendingMessagevalue: [
+
+            {
+                messageimg: "https://images.unsplash.com/flagged/photo-1553642618-de0381320ff3?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "hello3"
+            },
+            {
+                messageimg: "https://images.unsplash.com/photo-1571513722275-4b41940f54b8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "namaste3"
+            },
+
+        ],
         age: 7,
         interests: [{
             icon: `<i class="ri-music-2-line"></i>`,
@@ -55,10 +90,21 @@ let users = [
         isFriend: null
     }, {
         profilePic: "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        displayPic: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPic: "https://images.unsplash.com/photo-1495385794356-15371f348c31?q=80&w=1570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         pendingMessage: "9",
         location: "ajmer",
-        name: "komal",
+        name: "komal", pendingMessagevalue: [
+
+            {
+                messageimg: "https://images.unsplash.com/photo-1618085219724-c59ba48e08cd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "hello4"
+            },
+            {
+                messageimg: "https://images.unsplash.com/photo-1495385794356-15371f348c31?q=80&w=1570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "namaste4"
+            },
+
+        ],
         age: 7,
         interests: [{
             icon: `<i class="ri-music-2-line"></i>`,
@@ -76,7 +122,18 @@ let users = [
         displayPic: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         pendingMessage: "6",
         location: "jaipur,india",
-        name: "ronika",
+        name: "ronika", pendingMessagevalue: [
+
+            {
+                messageimg: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "hello5"
+            },
+            {
+                messageimg: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                value: "namaste5"
+            },
+
+        ],
         age: 7,
         interests: [{
             icon: `<i class="ri-music-2-line"></i>`,
@@ -97,18 +154,41 @@ let isanimation = false;
 
 function setdata() {
     document.querySelector('.navbar img').src = users[curr].profilePic;
-    document.querySelector('.badge h5').textContent = users[curr].pendingMessage;
+    document.querySelector('.badge h5').textContent = users[curr].pendingMessagevalue.length;
     document.querySelector('.location h2').textContent = users[curr].location;
     document.querySelector('.name h2:nth-child(1)').textContent = users[curr].name;
     document.querySelector('.name h2:nth-child(2)').textContent = users[curr].age;
+
+    // let cardmessagedata1 = document.querySelector(".cardmessage-data img");
+    // let cardmessagedata2 = document.querySelector(".cardmessage-data p");
+
+    // cardmessagedata1.src = users[curr].profilePic;
+    // cardmessagedata2.textContent = users[curr].pendingMessagevalue
+    let cluter2 = "";
+users[curr].pendingMessagevalue.map((item)=>{
+    cluter2+= `
+    <h2 class="cardmessage-data w-[80vw] rounded m-5 flex items-center justify-between m-auto text-white text-2xl p-2 h-[15vw] bg-white/50 mt-2">
+    <div class="h-[2.9rem] w-[2.9rem] rounded-full overflow-hidden">
+        <img class="h-full w-full object-cover"
+            src=${item.messageimg}
+            alt="">
+    </div>
+
+    <p class="text-2xl p-2 text-black">${item.value}</p>
+
+</h2>
+    `
+    document.querySelector(".cardmessage").innerHTML = cluter2;
+})
+
     let cluter = "";
     users[curr].interests.map((item) =>
 
         cluter += ` <div class="tag flex items-center gap-2 rounded-full bg-white/50 px-5 py-2">
         ${item.icon}
 
-        <h2>${item.interest}</h2>  
-</div>`
+        <h2>${item.interest}</h2>   
+        </div>`
 
     )
     document.querySelector('.tags').innerHTML = cluter
@@ -122,6 +202,11 @@ function getdata() {
     curr = 2;
 }
 
+// let videocard = document.querySelector(".video-calling-card");
+// videocard.addEventListener("click",function(){
+//    videocard.classList.remove("z-[1]");
+
+// })
 
 function imagechange() {
     // agar animation nhi ho rha h to animation kro
@@ -162,7 +247,7 @@ function imagechange() {
             duration: 0.5
         })
         tl.from(".incomingcard", {
-            scale: 1.1,
+            scale: 1.4,
             opacity: 0,
             duration: 0.5
         })
@@ -173,7 +258,7 @@ let cross = document.querySelector('.cross');
 let accept = document.querySelector('.accept');
 cross.addEventListener('click', () => {
     imagechange();
-    setdata(curr-1);
+    setdata(curr - 1);
     gsap.from('.details-container .element', {
         y: "100%",
         opacity: 0,
@@ -184,22 +269,73 @@ cross.addEventListener('click', () => {
 })
 accept.addEventListener('click', () => {
     imagechange();
-    setdata(curr-1);
+    setdata(curr - 1);
     gsap.from('.details-container .element', {
         y: "100%",
         opacity: 0,
         stagger: 0.06,
         duration: 0.5
     })
-    
+
     // console.log('clicked')
 })
-gsap.to('.calling-icon',{
-    y:-20,
-    duration:.5,
-    yoyo:-1,
-    scrub:true,
-    repeat:Infinity
+gsap.to('.calling-icon', {
+    y: -20,
+    duration: .5,
+    yoyo: -1,
+    scrub: true,
+    repeat: Infinity
 
 })
 getdata();
+
+let chaticon = document.querySelector(".chaticon");
+chaticon.addEventListener('click', function () {
+    let messagecard = document.querySelector(".message-card");
+
+    messagecard.classList.remove("hidden");
+    messagecard.classList.add("block");
+    document.querySelector('.closemessage').addEventListener("click", function () {
+
+        messagecard.classList.remove("block");
+        messagecard.classList.add("hidden");
+    })
+    setTimeout(() => {
+        // messagecard.classList.remove("block");
+        // messagecard.classList.add("hidden");
+
+
+    }, 2500);
+})
+
+let callingicon = document.querySelector(".calling-icon");
+callingicon.addEventListener("click", function () {
+    let videocard = document.querySelector(".video-calling-card");
+    gsap.from(".video-calling-card", {
+        // y:20,
+        scale: 0,
+        duration: 0.9,
+    })
+    //    alert()
+    videocard.classList.remove("z-[1]");
+    videocard.classList.remove("hidden");
+    videocard.classList.add("z-[8]");
+    videocard.classList.add("block");
+
+    setTimeout(() => {
+        videocard.classList.remove("z-[8]");
+        videocard.classList.add("z-[1]");
+
+        videocard.classList.remove("block");
+        videocard.classList.add("hidden");
+
+
+
+    }, 2500);
+})
+
+// let cardmessagedata1 = document.querySelector(".cardmessage-data img");
+// let cardmessagedata2 = document.querySelector(".cardmessage-data p");
+
+// cardmessagedata1.src = users[curr].profilePic;
+// // cardmessagedata2.textContent ='newdata' ;
